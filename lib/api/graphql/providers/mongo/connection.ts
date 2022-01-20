2f00a41ea84d03755ffd63294d3c92391f6332ee
Arsam Sarabi <arsamsarabi@me.com>
2022-01-20 11:42:50 +0000

    production db connection settings

--- lib/api/graphql/providers/mongo/connection.ts
+++ lib/api/graphql/providers/mongo/connection.ts
@@ -3,13 +3,20 @@ import mongoose from "mongoose";
 const { NODE_ENV, DB_URL, DB_USERNAME, DB_PASSWORD } = process.env;
 
 export const connect = async () => {
-  const uri =
+  console.log("connecting to db in the clouds... ☁️");
+
+  console.log(`NODE_ENV: ${NODE_ENV}`);
+
+  const connectConfig =
     NODE_ENV === "production"
-      ? `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}${DB_URL}`
-      : DB_URL;
+      ? {
+          user: DB_USERNAME,
+          pass: DB_PASSWORD,
+        }
+      : {};
 
   await mongoose
-    .connect(String(uri))
+    .connect(String(DB_URL), connectConfig)
     .catch((err) => console.error(err))
     .then(() => {
       console.log(` 🛠  ${DB_URL} 🛠 `);
